f96a1471e057150f6c0dcf722d02af60d66a8e20
Rasail Shrestha <rasailshrestha@Rasails-MacBook-Air.local>
2020-11-05 19:55:40 -0500

    AngryBirds6

--- Bird.js
+++ Bird.js
@@ -2,12 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage= loadImage("sprites/smoke.png");
+    this.trajectory=[];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-
+  
     super.display();
+    if(this.body.velocity.x >10 && this.body.position.x > 100 ){
+    var position=[this.body.position.x, this.body.position.y];
+    this.trajectory.push(position);
+  }
+    for(var repeat=0;repeat < this.trajectory.length;repeat++){
+      image(this.smokeImage,this.trajectory[repeat][0],this.trajectory[repeat][1] )
+  
+    }
   }
 }
